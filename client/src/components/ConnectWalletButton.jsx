c1a94215cfffbb5d4291071e2a98286f1331d165
jaykomarraju <jay.komarraju@gmail.com>
2022-11-16 23:18:50 -0500

    Update ConnectWalletButton.jsx
    — Added Web3 package to connect wallet button

--- client/src/components/ConnectWalletButton.jsx
+++ client/src/components/ConnectWalletButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Web3 from "web3";
 
 const Button = styled.button`
   padding: 10px;
@@ -20,13 +21,9 @@ async function getAccount() {
   // return account;
   if (window.ethereum) {
     await window.ethereum.request({ method: "eth_requestAccounts" });
-
     window.web3 = new Web3(window.ethereum);
-
     const account = web3.eth.accounts;
-
     //Get the current MetaMask selected/active wallet
-
     const walletAddress = account.givenProvider.selectedAddress;
 
     console.log(`Wallet: ${walletAddress}`);
