81987fe20cb624cc272462060a28a2b26a373930
jaykomarraju <jay.komarraju@gmail.com>
2023-05-25 23:34:06 -0400

    Update ReceivedQuestionPlayground.jsx

--- client/src/components/ReceivedQuestionPlayground.jsx
+++ client/src/components/ReceivedQuestionPlayground.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import {db } from "../services/Firebase";
+import { db } from "../services/Firebase";
 import AnswerQuestion from "../pages/AnswerQuestion";
+import instance from "../contract";
 
 const Sect = styled.div`
   display: flex;
@@ -86,7 +87,6 @@ const Date = styled.p`
 `;
 
 const ReceivedQuestionPlayground = ({ walletAddress }) => {
-
   const navigate = useNavigate();
 
   const [questions, setQuestions] = useState([]);
@@ -108,52 +108,70 @@ const ReceivedQuestionPlayground = ({ walletAddress }) => {
 
     // WE HAVE TO FILTER THROUGH THE QUESTION WE RECEIVED AND ONLY SHOW THE ONES THAT ARE NOT DECLINED AND NOT ANSWERED
 
-    const questionsRef = db.collection("users").doc(walletAddress).collection("receivedQuestions");
+    const questionsRef = db
+      .collection("users")
+      .doc(walletAddress)
+      .collection("receivedQuestions");
     const snapshot = await questionsRef.get();
-    if (!snapshot.empty) {  // check if snapshot is not empty
+    if (!snapshot.empty) {
+      // check if snapshot is not empty
       const questions = [];
       snapshot.forEach((doc) => {
         const data = doc.data();
         if (!data.declined && !data.answered) {
-          questions.push({...data, id: doc.id});
+          questions.push({ ...data, id: doc.id });
         }
-      }
-      );
-      console.log('questions fetched:', questions); // log the fetched data
+      });
+      console.log("questions fetched:", questions); // log the fetched data
       setQuestions(questions);
     } else {
-      console.log('No documents found!');
+      console.log("No documents found!");
     }
-
-
-
-
   };
-  
+
   useEffect(() => {
     getQuestions();
-  }, []); 
+  }, []);
 
   const handleDecline = async (e, questionId) => {
     e.preventDefault();
     // console.log('questionId:', questionId);
-    const questionRef = db.collection("users").doc(walletAddress).collection("receivedQuestions").doc(questionId);
-  
+    const questionRef = db
+      .collection("users")
+      .doc(walletAddress)
+      .collection("receivedQuestions")
+      .doc(questionId);
+
+    // contract call
+    try {
+      await instance.methods
+        .declineQuestion(questionId)
+        .send({ from: walletAddress,
+          gas: 3000000, });
+    } catch (error) {
+      console.log(
+        "An error occurred while declining the question in the contract: ",
+        error
+      );
+      return;
+    }
+
     // set the question's declined field to true
     await questionRef.update({
-      declined: true
+      declined: true,
     });
-  
+
     // remove the question from the questions array
-    const newQuestions = questions.filter((question) => question.id !== questionId);
+    const newQuestions = questions.filter(
+      (question) => question.id !== questionId
+    );
     setQuestions(newQuestions);
   };
 
   const handleAnswer = async (e, questionId) => {
     e.preventDefault();
-    navigate('/answer', { state: { questionId, walletAddress } });
+    navigate("/answer", { state: { questionId, walletAddress } });
   };
-  
 
   return (
     <QuestionsPlayground>
@@ -170,13 +188,17 @@ const ReceivedQuestionPlayground = ({ walletAddress }) => {
             </Flexer2>
           </Sect>
           <Sect2>
-          <DeclineButton onClick={(e) => handleDecline(e, question.id)}>X</DeclineButton>
+            <DeclineButton onClick={(e) => handleDecline(e, question.id)}>
+              X
+            </DeclineButton>
 
             {/* <Link to="/ansques"> */}
 
-              {/* <ANSButton>ANSWER</ANSButton> */}
+            {/* <ANSButton>ANSWER</ANSButton> */}
 
-              <ANSButton onClick={(e) => handleAnswer(e, question.id)}>ANSWER</ANSButton>
+            <ANSButton onClick={(e) => handleAnswer(e, question.id)}>
+              ANSWER
+            </ANSButton>
             {/* </Link> */}
           </Sect2>
         </ReceivedQuestion>
